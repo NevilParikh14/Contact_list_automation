59af1bbe7b3ef13f093bd5def66374c86115c539
NevilParikh14 <92399024+NevilParikh14@users.noreply.github.com>
2023-10-11 23:13:37 +0530

    Update test_01_create_user.py

--- test_01_create_user.py
+++ test_01_create_user.py
@@ -10,39 +10,32 @@ class TestCreateUser(Base):
         # Call to create a user
         response_code, response = self.create_user()
 
-        # Verify that API call was successful
+        # Verify that the API call was successful
         assert response_code in [200, 201]
 
-        # Veirfy that user was created
+        # Veirfy that the user was created
         assert response['user']
 
-        # Verify that token is received that can be used for performin other actions
+        # Verify that a token is received that can be used for performing other actions
         assert response['token']
 
-        # Login and retrive the Bearer token
+        # Login and retrieve the Bearer token
         self.get_token(
             {"email": self.creds_file['users']['email'], "password": self.creds_file['users']['password']})
 
-        # Verify that token is received
+        # Verify that the token is received
         assert self.token
 
         # Logout the user
-        endpoint = "users/logout"
-        method = "POST"
-        header = {'Authorization': 'Bearer ' + self.token}
-        response_code, response = self.request(endpoint, method, header, {})
+        response_code, response = self.logout()
 
-        # Verify that user was able to logout successfuly
+        # Verify that the user was able to log out successfully
         assert response_code == 200
 
         self.token = ""
 
         # Create duplicate user
-        endpoint = "users"
-        method = "POST"
-        header = {'Content-Type': 'application/json'}
-        response_code, response = self.request(
-            endpoint, method, header, self.creds_file['users'])
+        response_code, response = self.create_user()
 
         # Verify that duplicate user cannot be created
         assert response_code == 400
